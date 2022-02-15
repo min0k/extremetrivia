028683f672293fb432a6b75a70de18b2020c3926
min0k <mino.kim@minokim.com>
2022-02-15 15:52:04 -0800

    add &amp; replace

--- src/utility/decoder.tsx
+++ src/utility/decoder.tsx
@@ -1,3 +1,3 @@
 export default function decoder(input : string) {
-    return input.replace(/&quot;/g, '"').replace(/&#039;/g, "'");
+    return input.replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&amp;/g, "'");
   }
