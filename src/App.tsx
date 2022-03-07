b8d8a79ef15c715a27f3598e61589e8ee01f450e
min0k <mino.kim@minokim.com>
2022-03-06 17:33:06 -0800

    Add about path

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
-import GamePage from "./pages/GamePage";
 import StartPage from "./pages/StartPage";
-import EndPage from "./pages/EndPage";
 import NavBar from "./components/NavBar";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -13,8 +12,8 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<StartPage />} />
-          <Route path="/trivia" element={<GamePage />} />
-          <Route path="/gameover" element={<EndPage />} />
+          <Route path="/trivia" element={<StartPage />} />
+          <Route path="/about" element={<About />} />
           <Route path="*" element={<StartPage />} />
         </Routes>
       </BrowserRouter>
